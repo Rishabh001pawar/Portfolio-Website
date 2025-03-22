ebe4f70bcb0be6e7c2b0d06370f847484a0f1909
Rishabh Farkade <rishabhpawar059@gmail.com>
2025-03-22 23:55:10 +0530

    dict

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/Portfolio-Website/', // Correct base path for GitHub Pages
   build: {
-    outDir: 'gallery', // Use 'dist' as the default output directory
+    outDir: 'dist', // Use 'dist' as the default output directory
   },
 });
