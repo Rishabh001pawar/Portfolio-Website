fba8aafa9ed9937252b2475d6fc25a66badaf450
Rishabh Farkade <rishabhpawar059@gmail.com>
2025-03-22 23:50:06 +0530

    initial commit for coorection

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/https://github.com/Rishabh001pawar/Portfolio-Website/',  // GitHub repo name with leading and trailing slashes
+  base: '/Portfolio-Website/', // Correct base path for GitHub Pages
   build: {
-    outDir: 'dist',
-  }
+    outDir: 'gallery', // Use 'dist' as the default output directory
+  },
 });
