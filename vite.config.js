e42b87c0fd6b906fba7d17a9b1bcec1db3cfa28f
Rishabh Farkade <rishabhpawar059@gmail.com>
2025-03-22 16:23:58 +0530

    deply

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/https://github.com/Rishabh001pawar/Portfolio-Website/',  // GitHub repo name with leading and trailing slashes
+  build: {
+    outDir: 'dist',
+  }
+});
